a230b0f9774e38b903c2c577f2d4af67f132f72a
akash <akash_sisodiya@persistent.co.in>
2018-01-17 16:52:31 +0530

    Animated Navbar Logo and Navbar Menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  menuitems: any[] = [{
+    "name": "Home",
+    "link": "/home"
+  }, {
+    "name": "Team",
+    "link": "/team"
+  }, {
+    "name": "Player",
+    "link": "/player"
+  }];
   constructor() { }
 
   ngOnInit() {
